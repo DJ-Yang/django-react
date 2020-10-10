6025b60e2a1e33afc101085036a483c497f7e399
DJ-Yang <dong3831@hanmail.net>
2020-10-10 18:28:16 +0900

    add custom converter code

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,5 +1,18 @@
-from django.urls import path, include
+from django.urls import path, include, register_converter
 from . import views
+
+# Converter 커스텀
+# class YearConverter:
+#   regex = '\d{4}'
+#   def to_python(self, value):
+#     return int(value)
+#   def to_url(self.value):
+#     return str(value)
+
+# register_converter(YearConverter, 'year')
+
+# <int:pk> 여기에서 int에 해당하는 것이 converter
+
 
 app_name='instagram'
 
