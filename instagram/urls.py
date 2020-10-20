7ba2da0b8eab7f4a7d6e36647591a7735e803ec8
DJ-Yang <dong3831@hanmail.net>
2020-10-20 15:19:06 +0900

    delete with form

--- instagram/urls.py
+++ instagram/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     # django form 강의
     path('new/', views.post_new, name='post_new'),
     path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -50,6 +50,17 @@ def post_edit(request, pk):
     'post': post,
   })
 
+@login_required
+def post_delete(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+
+  if request.method == 'POST':
+    post.delete()
+    messages.success(request, '포스팅을 삭제했습니다.')
+    return redirect('instagram:post_list')
+  return render(request, 'instagram/post_confirm_delete.html', {
+    'post': post,
+  })
 
 # 리스트 뷰 이용 방법
 # 방법 1
