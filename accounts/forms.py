ff3ef1c2f61e087df9471f6dbc49c5df93b4451b
DJ-Yang <dong3831@hanmail.net>
2020-10-23 15:23:03 +0900

    add quiz in login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,21 @@
 from django import forms
 from .models import Profile
 
+# login quiz
+from django.contrib.auth.forms import AuthenticationForm
+
 class ProfileForm(forms.ModelForm):
   class Meta:
     model = Profile
     fields = ['address', 'zipcode']
+
+class LoginForm(AuthenticationForm):
+  # 퀴즈 입력 필드
+  answer = forms.IntegerField(help_text = '3 + 3 = ?')
+
+  # 퀴즈 확인 부분
+  def clean_answer(self):
+    answer = self.cleaned_data.get('answer')
+    if answer != 6:
+      raise forms.ValidationError('땡~!')
+    return answer
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
 from . import views
+from .forms import LoginForm
 
 app_name='accounts'
 
 urlpatterns = [
   # path('login', LoginView.as_view(), name='login'),
-  path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'), #템플릿 네임 지정
+  # path('logout', LoginOutView.as_view(), name='logout'),
+  path('logout/', views.logout, name='logout'),
+  path('login/', LoginView.as_view(
+    form_class=LoginForm,
+    template_name='accounts/login_form.html'
+    ), name='login'), #템플릿 네임 지정
   path('profile/', views.profile, name='profile'),
   path('profile/edit/', views.profile_edit, name='profile_edit'),
+  path('signup/', views.signup, name='signup'),
 ]
