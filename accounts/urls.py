99447fd1f8b75e303d30682bc33135966ef89a42
DJ-Yang <dong3831@hanmail.net>
2020-10-23 15:07:10 +0900

    add profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
+from . import views
 
 app_name='accounts'
 
 urlpatterns = [
   # path('login', LoginView.as_view(), name='login'),
-  path('login', LoginView.as_view(template_name='accounts/login_form.html'), name='login'), #템플릿 네임 지정
+  path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'), #템플릿 네임 지정
+  path('profile/', views.profile, name='profile'),
+  path('profile/edit/', views.profile_edit, name='profile_edit'),
 ]
